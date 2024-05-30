fa0116d79d62c89dca8aa337ea0007c6a6c78bea
uzmahamid01 <uzma_hamid@tamu.edu>
2024-05-29 19:52:14 -0500

    files added

--- index.js
+++ index.js
@@ -1,20 +1,69 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-be834-default-rtdb.firebaseio.com/"
 }
 
+
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
-const addBtn = document.getElementById("add-button")
-const inputField = document.getElementById("input-field")
-
-addBtn.addEventListener("click", function() {
-    let inputValue = inputField.value
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
     push(shoppingListInDB, inputValue)
-    console.log(inputValue)
+    
+    clearInputFieldEl()
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    
+    // Challenge: Change the onValue code so that it uses snapshot.exists() 
+    //to show items when there are items in the database and if there are not displays 
+    //the text 'No items here... yet'.
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+        clearShoppingListEl()
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToShoppingListEl(currentItem)
+        }
+    }
+    else {
+        shoppingListEl.innerHTML = "No items here... yet"
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        
+        remove(exactLocationOfItemInDB)
+    })
+    
+    shoppingListEl.append(newEl)
+}
